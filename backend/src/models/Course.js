fe2550e17c397aaee4c554c4feccfc342e94d8d4
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-12-28 15:24:42 +0530

    add course schema

---
+++ backend/src/models/Course.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const courseSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    assignedTeacher: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    enrolledStudents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+}, { timestamps: true });
+
+export const Course = mongoose.model('Course', courseSchema);
